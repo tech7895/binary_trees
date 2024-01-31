152c5f065e2da1ab8fc308c7998f4da8979d18b6
tech7895 <techhunter990@gmail.com>
2024-01-31 01:59:15 -0800

    adv

--- binary_trees.h
+++ binary_trees.h
@@ -130,6 +130,9 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 /* 130-binary_tree_is_heap.c */
 int binary_tree_is_heap(const binary_tree_t *tree);
