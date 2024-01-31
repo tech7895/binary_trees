ce20fb329a3d95d41b949025622a25060ecb27c8
tech7895 <techhunter990@gmail.com>
2024-01-31 02:09:51 -0800

    adv

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,55 +1,107 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
+q_node *enqueue(binary_tree_t *node, q_node **head, q_node **tail);
+void free_queue(q_node *head);
+
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree.
- * @tree: pointer to the root node of the tree to check.
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  *
- * If tree is NULL, return 0.
+ * Description: This solution uses a queue to store the nodes in inorder then
+ * checks if the queue is sorted in ascending order. Inorder traversal of a
+ * binary search tree should be sorted in ascending order.
+ * The queue is implemented using a linked list.
+ * Time complexity: O(n)
+ * Space complexity: O(n)
  *
+ * @tree: pointer to the root node of the tree to check
  * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	/* If the tree is empty, it's not a BST. */
-	if (tree == NULL)
+	q_node *head = NULL, *tail = NULL, *current = NULL;
+
+	if (!tree)
 		return (0);
-	/* If the tree has no children, it's a BST. */
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	/* If the tree has two children, */
-	if (tree->left != NULL && tree->right != NULL)
+
+	create_queue((binary_tree_t *)tree, &head, &tail, &enqueue);
+
+	current = head;
+
+	while (current->next)
 	{
-		/*
-		* check that left child is less than the parent and the right
-		* child is greater than the parent,
-		*/
-		if (tree->left->n < tree->n && tree->right->n > tree->n)
-			/* If so, recursively check that left and right subtrees are also BSTs */
-			return (binary_tree_is_bst(tree->left) &&
-				binary_tree_is_bst(tree->right));
-		else
+		if (current->b_tree_node->n >= current->next->b_tree_node->n)
+		{
+			free_queue(head);
 			return (0);
+		}
+		current = current->next;
 	}
-	/* If the tree has only a left child, */
-	if (tree->left != NULL)
-	{
-		/* check that the left child is less than the parent. */
-		if (tree->left->n < tree->n)
-			/* If so, recursively check that the left subtree is a BST. */
-			return (binary_tree_is_bst(tree->left));
-		else
-			return (0);
-	}
-	if (tree->right != NULL) /* If the tree has only a right child, */
-	{
-		/* check that the right child is greater than the parent. */
-		if (tree->right->n > tree->n)
-			/* If so, recursively check that the right subtree is a BST. */
-			return (binary_tree_is_bst(tree->right));
-		else
-			return (0);
-	}
-	return (0);
+	free_queue(head);
+	return (1);
 }
 
-/* CODE DOESN'T PASS ALL CHECKS */
+/**
+ * create_queue - creates a queue of nodes in inorder traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @head: pointer to the head of the queue
+ * @tail: pointer to the tail of the queue
+ * @func: pointer to a function to call for each node
+ * Return: void
+ */
+void create_queue(binary_tree_t *tree, q_node **head, q_node **tail,
+				  q_node *(*func)(binary_tree_t *, q_node **, q_node **))
+{
+	if (!tree || !func)
+		return;
+	create_queue(tree->left, head, tail, func);
+	func(tree, head, tail);
+	create_queue(tree->right, head, tail, func);
+}
+
+/**
+ * enqueue - adds a node to the end of a queue
+ * @node: pointer to the binary tree node
+ * @head: pointer to the head of the queue
+ * @tail: pointer to the tail of the queue
+ * Return: pointer to the new node
+ */
+q_node *enqueue(binary_tree_t *node, q_node **head, q_node **tail)
+{
+	q_node *new_node = NULL;
+
+	if (!node || !head || !tail)
+		return (NULL);
+	new_node = malloc(sizeof(q_node));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->b_tree_node = node;
+	new_node->next = NULL;
+
+	if (!*head)
+		*head = new_node;
+	if (*tail)
+		(*tail)->next = new_node;
+	*tail = new_node;
+	return (new_node);
+}
+
+/**
+ * free_queue - frees a queue
+ * @head: pointer to the head of the queue
+ * Return: void
+ */
+void free_queue(q_node *head)
+{
+	q_node *temp = NULL;
+
+	while (head)
+	{
+
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
