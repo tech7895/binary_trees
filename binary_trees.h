69334eb96974664f845698a6e2353e5ea03da540
tech7895 <techhunter990@gmail.com>
2024-01-31 03:09:35 -0800

    adv

--- binary_trees.h
+++ binary_trees.h
@@ -97,8 +97,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* 100-binary_trees_ancestor.c */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 /* 100-binary_trees_ancestor.c */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
